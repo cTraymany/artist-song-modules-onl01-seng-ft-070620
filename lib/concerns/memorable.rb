a3d8de738abe01675a1ebce0174318088b0f28b2
Chindalath Traymany <chindalatht@gmail.com>
2020-07-22 11:56:45 +0000

    Automatically backed up by Learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+  def ClassMethods
+    def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
+  end
+  
+  def InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
